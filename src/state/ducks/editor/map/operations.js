8dc77c34724d43c766de46e894aa939a94aeceef
Jim <43685375+jim-lub@users.noreply.github.com>
2019-10-15 18:04:13 +0200

    fixed major bug causing editor to crash

--- src/state/ducks/editor/map/operations.js
+++ src/state/ducks/editor/map/operations.js
@@ -62,11 +62,15 @@ export const initializeMap = () => (dispatch, getState) => {
 
     // mapGrid
     .then(mapProperties => {
+      console.log(currentScene)
       return dispatch( firestore.getMapGrid({ uid: currentScene.uid }) )
-        .then(mapGrid => (mapGrid)
-          ? dispatch( actions.setMapGrid({ mapGrid }) )
-          : dispatch( actions.setMapGrid({ mapGrid: utils.buildMapGrid({ mapSize: mapProperties.mapSize }) })
-        ))
+        .then(mapGrid => {
+          // console.log()
+          return (!!mapGrid)
+            ? dispatch( actions.setMapGrid({ mapGrid }) )
+            : dispatch( actions.setMapGrid({ mapGrid: utils.buildMapGrid({ mapSize: mapProperties.mapSize }) }))
+        }
+        )
     })
 
     // tilemapData
