b8bfa232313cd5e5b38e500fccb0f33b21406358
Jim <43685375+jim-lub@users.noreply.github.com>
2019-10-29 18:19:58 +0100

    breakpoints

--- src/lib/constants/editorModuleDefaultLayouts.js
+++ src/lib/constants/editorModuleDefaultLayouts.js
@@ -1,64 +1,133 @@
+import breakpoints from './breakpoints';
 import * as moduleTypes from './editorModuleTypes';
 
 export default {
-  breakpoints: {
-    xl: 1600,
-    lg: 1200,
-    md: 996,
-    sm: 768,
-    xs: 480,
-    xxs: 0
-  },
+  breakpoints,
 
   columns: {
-    xl: 16,
-    lg: 12,
-    md: 10,
-    sm: 8,
+    xxl: 100,
+    // xl: 16,
+    lg: 100,
+    // md: 10,
+    sm: 20,
     xs: 6,
-    xxs: 4
   },
 
-  rowHeight: 30
+  rowHeight: 20
 }
 
 export const defaultLayout = {
-  xl: [
-    {i: moduleTypes.colorPicker, x: 0, y: 0, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.layers, x: 0, y: 1, w: 2, h: 14, minW: 2, minH: 5},
-    {i: moduleTypes.map, x: 3, y: 0, w: 13, h: 25, minW: 2, minH: 5},
-    {i: moduleTypes.properties, x: 0, y: 16, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.toolbar, x: 0, y: 0, w: 2, h: 4, minW: 2, minH: 4, maxW: 2, maxH: 4},
+  xxl: [
+    {
+      i: moduleTypes.colorPicker,
+      x: 0, y: 0,
+      w: 8, h: 10,
+      minW: 4, minH: 8,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.layers,
+      x: 0, y: 14,
+      w: 8, h: 20,
+      minW: 4, minH: 8,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.map,
+      x: 8, y: 0,
+      w: 92, h: 40,
+      minW: 10, minH: 10,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.properties,
+      x: 0, y: 34,
+      w: 8, h: 6,
+      minW: 8, minH: 6,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.toolbar,
+      x: 0, y: 10,
+      w: 8, h: 4,
+      minW: 4, minH: 4,
+      // maxW: 8, maxH: 4
+    },
   ],
+
   lg: [
-    {i: moduleTypes.colorPicker, x: 0, y: 0, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.layers, x: 0, y: 1, w: 2, h: 14, minW: 2, minH: 5},
-    {i: moduleTypes.map, x: 3, y: 0, w: 10, h: 25, minW: 2, minH: 5},
-    {i: moduleTypes.properties, x: 0, y: 16, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.toolbar, x: 2, y: 0, w: 1, h: 8, minW: 1, minH: 2, maxW: 4, maxH: 8},
+    {
+      i: moduleTypes.colorPicker,
+      x: 0, y: 0,
+      w: 16, h: 10,
+      minW: 4, minH: 8,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.layers,
+      x: 0, y: 14,
+      w: 16, h: 18,
+      minW: 4, minH: 8,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.map,
+      x: 16, y: 0,
+      w: 84, h: 36,
+      minW: 10, minH: 10,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.properties,
+      x: 0, y: 32,
+      w: 16, h: 6,
+      minW: 8, minH: 6,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.toolbar,
+      x: 0, y: 10,
+      w: 16, h: 4,
+      minW: 4, minH: 4,
+      // maxW: 8, maxH: 4
+    },
   ],
-  md: [
-    {i: moduleTypes.colorPicker, x: 0, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.layers, x: 2, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.map, x: 0, y: 0, w: 10, h: 25, minW: 2, minH: 5},
-    {i: moduleTypes.properties, x: 4, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-  ],
+
   sm: [
-    {i: moduleTypes.colorPicker, x: 0, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.layers, x: 2, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.map, x: 0, y: 0, w: 8, h: 25, minW: 2, minH: 5},
-    {i: moduleTypes.properties, x: 4, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-  ],
-  xs: [
-    {i: moduleTypes.colorPicker, x: 0, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.layers, x: 2, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.map, x: 0, y: 0, w: 6, h: 25, minW: 2, minH: 5},
-    {i: moduleTypes.properties, x: 4, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-  ],
-  xxs: [
-    {i: moduleTypes.colorPicker, x: 0, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.layers, x: 2, y: 1, w: 2, h: 8, minW: 2, minH: 5},
-    {i: moduleTypes.map, x: 0, y: 0, w: 4, h: 25, minW: 2, minH: 5},
-    {i: moduleTypes.properties, x: 4, y: 2, w: 2, h: 8, minW: 2, minH: 5},
+    {
+      i: moduleTypes.colorPicker,
+      x: 0, y: 15,
+      w: 8, h: 10,
+      minW: 2, minH: 2,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.layers,
+      x: 8, y: 15,
+      w: 9, h: 10,
+      minW: 2, minH: 2,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.map,
+      x: 0, y: 0,
+      w: 20, h: 15,
+      minW: 10, minH: 5,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.properties,
+      x: 0, y: 25,
+      w: 8, h: 10,
+      minW: 2, minH: 2,
+      // maxW: 0, maxH: 0
+    },
+    {
+      i: moduleTypes.toolbar,
+      x: 17, y: 15,
+      w: 3, h: 10,
+      minW: 2, minH: 2,
+      // maxW: 8, maxH: 4
+    },
   ],
 }
