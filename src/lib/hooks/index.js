ab16ce0299925f4bd8a74c7162a7447c6e6e0cb5
Jim <43685375+jim-lub@users.noreply.github.com>
2019-11-27 16:43:27 +0100

    deleted unused files

--- src/lib/hooks/index.js
+++ src/lib/hooks/index.js
@@ -2,4 +2,3 @@ export { useAsyncRequestHelper } from './useAsyncRequestHelper';
 export { useModal } from './useModal';
 export { useContextMenu } from './useContextMenu';
 export { useKeyPress } from './useKeyPress';
-export { useStateRef } from './useStateRef';
--- src/lib/hooks/useStateRef/index.js
+++
@@ -1,1 +0,0 @@
-export { useStateRef } from './useStateRef';
